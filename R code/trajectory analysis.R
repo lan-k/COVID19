dc26b1af68a161c8c0263af2684492d620bd2cf6
lan-k <60991703+lan-k@users.noreply.github.com>
2020-06-29 15:38:25 +0930

    Add files via upload

--- R code/trajectory analysis.R
+++ R code/trajectory analysis.R
@@ -16,8 +16,13 @@ source('D:/work/SAHMRI/COVID-19/R code/age adjusted weights international.R')
 
 plot_death <- function(country) {
   df <- deaths %>% filter(location == country)
+  par(mfrow=c(1,2))
+  plot(df$day, df$death_rate_week)
+  lines(df$day, df$death_rate_smooth, col="red")
+  
   plot(df$day, df$total_death_rate)
   lines(df$day, df$total_death_rate_smooth, col="red")
+  
   
 }
 
@@ -33,10 +38,6 @@ plot_death <- function(country) {
 #   select(Country, Country.code) %>%
 #   rename(country=Country)
 # 
-UN_iso_codes <- read.csv("Country UN ISO codes.csv") %>%
-  rename(ISO_country=country)  %>%
-  filter(ISO3 != "OWID_WRL") %>%
-  mutate(ISO3=substr(ISO3,1,3))
 
 # 
 # weights_ISO <-  weights_all %>%
@@ -47,7 +48,15 @@ UN_iso_codes <- read.csv("Country UN ISO codes.csv") %>%
 #   mutate(ISO3=substr(ISO3,1,3))
 # 
 # save(weights_ISO, file="weights_ISO_China.Rdata")
+
 load("weights_ISO_China.Rdata")
+
+
+UN_iso_codes <- read.csv("Country UN ISO codes.csv") %>%
+  rename(ISO_country=country)  %>%
+  filter(ISO3 != "OWID_WRL") %>%
+  mutate(ISO3=substr(ISO3,1,3))
+
 
 minfu <- 90  #minimum number of days after first death is recorded
 mindeath <- 50  #exclude regions with total number of deaths less than this
@@ -61,10 +70,12 @@ maxday <- paste0("day",minfu)
 #   select(Date, cases, deaths, popData2018)  #fileEncoding = "UTF-8-BOM",
 # countries <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/ecdc/locations.csv",
 #                       stringsAsFactors = F,na.strings = "")
+# owid <-  read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv",
+#   stringsAsFactors = F,na.strings = "") 
 
+owid <-  read.csv('https://covid.ourworldindata.org/data/owid-covid-data.csv',
+                  stringsAsFactors = F,na.strings = "")                 
 
-owid <-  read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv",
-stringsAsFactors = F,na.strings = "") 
 
 deaths <- owid %>%
   mutate( Date = as.Date(date, format = "%Y-%m-%d")) %>%
@@ -72,7 +83,8 @@ deaths <- owid %>%
   left_join(weights_ISO, by=c("iso_code" = "ISO3")) %>%
   filter(!is.na(weight_death)) %>%
   group_by(location) %>%
-  mutate(
+  mutate(mindate = min(Date),
+    day = row_number(), #Date-mindate-1,
     new_deaths = replace_na(new_deaths, 0),
     total_deaths = cumsum(new_deaths),
     maxdeath=max(total_deaths),
@@ -84,28 +96,57 @@ deaths <- owid %>%
     x = new_deaths, 
     k = 7, 
     idx = Date, na_rm = TRUE),
-    mean_deaths_14 = mean_run(
-      x = new_deaths, 
-      k = 14, 
+    total_deaths_7 = mean_run(
+      x = total_deaths, 
+      k = 7, 
       idx = Date, na_rm = TRUE),
     death_rate_week = weight_death*1000000*mean_deaths_7/population,
-    death_rate_14 = weight_death*1000000*mean_deaths_14/population,
-    total_death_rate=weight_death*1000000*total_deaths/population,
-    total_death_rate_smooth = as.vector(smooth(total_death_rate, kind="S", twiceit=T)),
-    deaths_smooth=as.vector(smooth(mean_deaths_7, kind="S", twiceit=T)),
-    death_rate_smooth = weight_death*1000000*deaths_smooth/population,
+    total_death_rate=weight_death*1000000*total_deaths_7/population,
+    #total_death_rate_smooth = as.vector(smooth(total_death_rate, kind="S", twiceit=T)),
+    #deaths_smooth=as.vector(smooth(mean_deaths_7, kind="S", twiceit=T)),
+    #death_rate_smooth = weight_death*1000000*mean_deaths_7/population,
     deathdays =total_deaths >= 1) %>%
+  ungroup()
+
+n_nation <- unique(deaths$location)
+
+###spline fit to total death rate
+total_death_rate_smooth <- NULL
+for (i in seq(length(n_nation))) {
+  nation <- deaths %>% filter(location == n_nation[i])
+ 
+  spl <- with(nation, smooth.spline(day,total_death_rate))
+  nation$total_death_rate_smooth <- spl$y
+  nation$total_death_rate_smooth[nation$total_death_rate_smooth <0] = 0
+  
+  spl2 <- with(nation, smooth.spline(day,death_rate_week))
+  nation$death_rate_smooth <- spl2$y
+  nation$death_rate_smooth[nation$death_rate_smooth <0] = 0
+  
+  
+  nation <- nation %>%
+    select(Date, iso_code,death_rate_smooth, total_death_rate_smooth)
+  
+  total_death_rate_smooth <- bind_rows(total_death_rate_smooth, nation)
+ 
+}
+
+
+deaths <- deaths %>%
+  left_join(total_death_rate_smooth, by = c("Date","iso_code")) %>%
+  group_by(location) %>%
   filter(deathdays & maxdeath >= mindeath) %>%
   mutate(day=row_number(),
-         maxfu = max(day)
-         )  %>%
+         maxfu = max(day),
+  )  %>%
   ungroup() %>%
   select(-deathdays)
 
 summary(deaths$maxfu)
 summary(deaths$mean_deaths_3)
-summary(deaths$deaths_smooth)
 summary(deaths$death_rate_smooth)
+summary(deaths$total_death_rate_smooth)
+
 
 plot_death("Brazil") 
 
@@ -176,7 +217,7 @@ plot(s3)
 plotMeanTraj(s3, ylim=c(0,3000))
 plotMedTraj(s3, ylim=c(0,3000))
 
-###using weighted death rate
+###using weighted death rate  - mean deaths past 7 days
 death_rate <- deaths %>%
   filter(maxfu >= minfu & day <= minfu) %>%
   select(day, iso_code, death_rate_smooth) %>%
@@ -208,7 +249,7 @@ death_rate_clusters <- death_rate %>%
   left_join(IDs,by="ID" )
 
 r3$clust.distr
-death_rate_clusters %>% filter(cluster !=3) %>% select(cluster,ISO_country)
+death_rate_clusters %>% filter(cluster !=2) %>% select(cluster,ISO_country)
 
 
 head(clusters)
@@ -218,11 +259,11 @@ maxclust <- max(death_rate_clusters$cluster)
 
 plot(r3, ylim=c(0,15))
 
-plotMeanTraj(r3, ylim=c(0,8))
-plotMedTraj(r3, ylim=c(0,8))
+plotMeanTraj(r3, ylim=c(0,2))
+plotMedTraj(r3, ylim=c(0,5))
 plotBoxplotTraj(r3)
 plotCombTraj(r3)  #
-plotCombTraj(r3,stat.type = "median", ylim=c(0,8), col=1:maxclust) 
+plotCombTraj(r3,stat.type = "median", ylim=c(0,1), col=1:maxclust) 
 legend("topleft", legend=1:maxclust,
        pch=1:maxclust,lty=1:maxclust, col=1:maxclust)
 
@@ -251,7 +292,7 @@ t2 = step2factors(t1)
 head(t2$factors)
 
 t3 = step3clusters(t2)  
-t3 = step3clusters(r2,nclusters = 4)
+#t3 = step3clusters(r2,nclusters = 4)
 
 clusters <- t3$clusters
 (maxclust <- max(clusters$cluster))
@@ -262,7 +303,8 @@ total_death_rate_clusters <- death_rate %>%
   left_join(IDs,by="ID" )
 
 t3$clust.distr
-total_death_rate_clusters %>% filter(cluster !=1) %>% select(cluster,ISO_country)
+total_death_rate_clusters %>% filter(cluster %in% c(1,2)) %>% select(cluster,ISO_country)
+total_death_rate_clusters %>% filter(cluster ==3) %>% select(cluster,ISO_country)
 
 head(clusters)
 summary(t3)
@@ -270,10 +312,10 @@ summary(t3)
 plot(t3)
 
 plotMeanTraj(t3, ylim=c(0,400))
-plotMedTraj(t3, ylim=c(0,400))
+plotMedTraj(t3, ylim=c(0,500))
 plotBoxplotTraj(t3)
 plotCombTraj(t3)  #
-plotCombTraj(t3,ylim=c(0,300), col=1:maxclust) 
+plotCombTraj(t3,ylim=c(0,350), col=1:maxclust) 
 legend("topleft", legend=1:maxclust,
        pch=1:maxclust,lty=1:maxclust, col=1:maxclust)
 
