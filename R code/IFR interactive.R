30b2bd82b8f1240e444c362cd8b323dcb0615407
lan-k <60991703+lan-k@users.noreply.github.com>
2020-08-05 20:44:47 +0930

    Update and rename IFR interactive to IFR interactive.R

---
+++ R code/IFR interactive.R
@@ -0,0 +1,19 @@
+library(tmap)
+
+data("World")
+
+load(file=".\Data\IFR_wt_cluster.Rdata")
+clust <- World %>%
+  left_join(weights_ISO_clust, by=c("iso_a3"="ISO3")) %>%
+  mutate(IFR=round(100*IFR, digits=2))
+  
+tmap_mode("view")  #interactive
+
+tm <- tm_shape(clust) +
+  tm_polygons("IFR", legend.title = "IFR (%)", colorNA = "white", drop.levels = T,
+              palette="YlOrRd", inner.margins = c(0, .02, .02, .02), alpha=.7)  #-RdYlGn
+
+tm
+
+## save as stand-alone HTML file ("view" mode)
+tmap_save(tm, filename = "IFR world_map.html")
